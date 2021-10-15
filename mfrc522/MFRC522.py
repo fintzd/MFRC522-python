437aac2a7a5a37ca80051e338a80553dcfe7b561
fintzd <35842457+fintzd@users.noreply.github.com>
2021-10-15 15:10:06 +0200

    Update MFRC522.py

--- mfrc522/MFRC522.py
+++ mfrc522/MFRC522.py
@@ -139,5 +139,18 @@ class MFRC522:
         GPIO.output(self.RSTGPIO, 1)
         # initialise
         self.MFRC522_Init()
-
         
+        
+    def Reset_MFRC522(self):
+        self.Write_MFRC522(self.CommandReg, self.PCD_RESETPHASE)
+    
+    
+    def Write_MFRC522(self, addr, val):
+        val = self.spi.xfer2([(addr << 1) & 0x7E, val])
+    
+    
+    def Read_MFRC522(self, addr):
+        val = self.spi.xfer2([((addr << 1) & 0x7E) | 0x80, 0])
+        return val[1]
+
+
