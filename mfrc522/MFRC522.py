d8ea3f81ca65e0d8339b13bafa7c7d85193ac4aa
fintzd <35842457+fintzd@users.noreply.github.com>
2021-10-17 11:16:07 +0200

    Update MFRC522.py

--- mfrc522/MFRC522.py
+++ mfrc522/MFRC522.py
@@ -26,103 +26,103 @@ import spidev
 
 
 class MFRC522:
-    RSTGPIO             = 25
+    RSTGPIO = 25
 
-    MAX_LEN             = 16
+    MAX_LEN = 16
 
-    PCD_IDLE            = 0x00
-    PCD_AUTHENT         = 0x0E
-    PCD_RECEIVE         = 0x08
-    PCD_TRANSMIT        = 0x04
-    PCD_TRANSCEIVE      = 0x0C
-    PCD_RESETPHASE      = 0x0F
-    PCD_CALCCRC         = 0x03
+    PCD_IDLE = 0x00
+    PCD_AUTHENT = 0x0E
+    PCD_RECEIVE = 0x08
+    PCD_TRANSMIT = 0x04
+    PCD_TRANSCEIVE = 0x0C
+    PCD_RESETPHASE = 0x0F
+    PCD_CALCCRC = 0x03
 
-    PICC_REQIDL         = 0x26
-    PICC_REQALL         = 0x52
-    PICC_ANTICOLL       = 0x93
-    PICC_SELECTTAG      = 0x93
-    PICC_AUTHENT1A      = 0x60
-    PICC_AUTHENT1B      = 0x61
-    PICC_READ           = 0x30
-    PICC_WRITE          = 0xA0
-    PICC_DECREMENT      = 0xC0
-    PICC_INCREMENT      = 0xC1
-    PICC_RESTORE        = 0xC2
-    PICC_TRANSFER       = 0xB0
-    PICC_HALT           = 0x50
+    PICC_REQIDL = 0x26
+    PICC_REQALL = 0x52
+    PICC_ANTICOLL = 0x93
+    PICC_SELECTTAG = 0x93
+    PICC_AUTHENT1A = 0x60
+    PICC_AUTHENT1B = 0x61
+    PICC_READ = 0x30
+    PICC_WRITE = 0xA0
+    PICC_DECREMENT = 0xC0
+    PICC_INCREMENT = 0xC1
+    PICC_RESTORE = 0xC2
+    PICC_TRANSFER = 0xB0
+    PICC_HALT = 0x50
 
-    MI_OK               = 0
-    MI_NOTAGERR         = 1
-    MI_ERR              = 2
+    MI_OK = 0
+    MI_NOTAGERR = 1
+    MI_ERR = 2
 
-    Reserved00          = 0x00
-    CommandReg          = 0x01
-    CommIEnReg          = 0x02
-    DivlEnReg           = 0x03
-    CommIrqReg          = 0x04
-    DivIrqReg           = 0x05
-    ErrorReg            = 0x06
-    Status1Reg          = 0x07
-    Status2Reg          = 0x08
-    FIFODataReg         = 0x09
-    FIFOLevelReg        = 0x0A
-    WaterLevelReg       = 0x0B
-    ControlReg          = 0x0C
-    BitFramingReg       = 0x0D
-    CollReg             = 0x0E
-    Reserved01          = 0x0F
+    Reserved00 = 0x00
+    CommandReg = 0x01
+    CommIEnReg = 0x02
+    DivlEnReg = 0x03
+    CommIrqReg = 0x04
+    DivIrqReg = 0x05
+    ErrorReg = 0x06
+    Status1Reg = 0x07
+    Status2Reg = 0x08
+    FIFODataReg = 0x09
+    FIFOLevelReg = 0x0A
+    WaterLevelReg = 0x0B
+    ControlReg = 0x0C
+    BitFramingReg = 0x0D
+    CollReg = 0x0E
+    Reserved01 = 0x0F
 
-    Reserved10          = 0x10
-    ModeReg             = 0x11
-    TxModeReg           = 0x12
-    RxModeReg           = 0x13
-    TxControlReg        = 0x14
-    TxAutoReg           = 0x15
-    TxSelReg            = 0x16
-    RxSelReg            = 0x17
-    RxThresholdReg      = 0x18
-    DemodReg            = 0x19
-    Reserved11          = 0x1A
-    Reserved12          = 0x1B
-    MifareReg           = 0x1C
-    Reserved13          = 0x1D
-    Reserved14          = 0x1E
-    SerialSpeedReg      = 0x1F
+    Reserved10 = 0x10
+    ModeReg = 0x11
+    TxModeReg = 0x12
+    RxModeReg = 0x13
+    TxControlReg = 0x14
+    TxAutoReg = 0x15
+    TxSelReg = 0x16
+    RxSelReg = 0x17
+    RxThresholdReg = 0x18
+    DemodReg = 0x19
+    Reserved11 = 0x1A
+    Reserved12 = 0x1B
+    MifareReg = 0x1C
+    Reserved13 = 0x1D
+    Reserved14 = 0x1E
+    SerialSpeedReg = 0x1F
 
-    Reserved20          = 0x20
-    CRCResultRegM       = 0x21
-    CRCResultRegL       = 0x22
-    Reserved21          = 0x23
-    ModWidthReg         = 0x24
-    Reserved22          = 0x25
-    RFCfgReg            = 0x26
-    GsNReg              = 0x27
-    CWGsPReg            = 0x28
-    ModGsPReg           = 0x29
-    TModeReg            = 0x2A
-    TPrescalerReg       = 0x2B
-    TReloadRegH         = 0x2C
-    TReloadRegL         = 0x2D
-    TCounterValueRegH   = 0x2E
-    TCounterValueRegL   = 0x2F
+    Reserved20 = 0x20
+    CRCResultRegM = 0x21
+    CRCResultRegL = 0x22
+    Reserved21 = 0x23
+    ModWidthReg = 0x24
+    Reserved22 = 0x25
+    RFCfgReg = 0x26
+    GsNReg = 0x27
+    CWGsPReg = 0x28
+    ModGsPReg = 0x29
+    TModeReg = 0x2A
+    TPrescalerReg = 0x2B
+    TReloadRegH = 0x2C
+    TReloadRegL = 0x2D
+    TCounterValueRegH = 0x2E
+    TCounterValueRegL = 0x2F
 
-    Reserved30          = 0x30
-    TestSel1Reg         = 0x31
-    TestSel2Reg         = 0x32
-    TestPinEnReg        = 0x33
-    TestPinValueReg     = 0x34
-    TestBusReg          = 0x35
-    AutoTestReg         = 0x36
-    VersionReg          = 0x37
-    AnalogTestReg       = 0x38
-    TestDAC1Reg         = 0x39
-    TestDAC2Reg         = 0x3A
-    TestADCReg          = 0x3B
-    Reserved31          = 0x3C
-    Reserved32          = 0x3D
-    Reserved33          = 0x3E
-    Reserved34          = 0x3F
+    Reserved30 = 0x30
+    TestSel1Reg = 0x31
+    TestSel2Reg = 0x32
+    TestPinEnReg = 0x33
+    TestPinValueReg = 0x34
+    TestBusReg = 0x35
+    AutoTestReg = 0x36
+    VersionReg = 0x37
+    AnalogTestReg = 0x38
+    TestDAC1Reg = 0x39
+    TestDAC2Reg = 0x3A
+    TestADCReg = 0x3B
+    Reserved31 = 0x3C
+    Reserved32 = 0x3D
+    Reserved33 = 0x3E
+    Reserved34 = 0x3F
 
     serNum = []
 
@@ -230,7 +230,6 @@ class MFRC522:
                 status = self.MI_ERR
         return (status, backData, backLen)
 
-
     def Request_MFRC522(self, reqMode):
         status = None
         backBits = None
@@ -239,13 +238,13 @@ class MFRC522:
         self.Write_MFRC522(self.BitFramingReg, 0x07)
         TagType.append(reqMode)
 
-        (status, backData, backBits) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, TagType)
+        (status, backData, backBits) = self.Communicate_MFRC522(
+            self.PCD_TRANSCEIVE, TagType)
 
         if ((status != self.MI_OK) | (backBits != 0x10)):
             status = self.MI_ERR
 
         return (status, backBits)
-
 
     def Anticoll_MFRC522(self):
         backData = []
@@ -257,7 +256,8 @@ class MFRC522:
         serNum.append(self.PICC_ANTICOLL)
         serNum.append(0x20)
 
-        (status, backData, backBits) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, serNum)
+        (status, backData, backBits) = self.Communicate_MFRC522(
+            self.PCD_TRANSCEIVE, serNum)
 
         if (status == self.MI_OK):
             if len(backData) == 5:
@@ -268,7 +268,6 @@ class MFRC522:
             else:
                 status = self.MI_ERR
         return (status, backData)
-
 
     def CalulateCRC_MFRC522(self, pIndata):
         self.ClearBitMask_MFRC522(self.DivIrqReg, 0x04)
@@ -291,7 +290,6 @@ class MFRC522:
         pOutData.append(self.Read_MFRC522(self.CRCResultRegM))
         return pOutData
 
-
     def SelectTag_MFRC522(self, serNum):
         backData = []
         buff = []
@@ -306,12 +304,12 @@ class MFRC522:
         buff.append(pOut[0])
         buff.append(pOut[1])
 
-        (status, backData, backLen) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, buff)
+        (status, backData, backLen) = self.Communicate_MFRC522(
+            self.PCD_TRANSCEIVE, buff)
 
         if (status == self.MI_OK) and (backLen == 0x18):
             return backData[0]
         return 0
-
 
     def Auth_MFRC522(self, authMode, BlockAddr, Sectorkey, serNum):
         buff = []
@@ -324,13 +322,12 @@ class MFRC522:
         for i in range(4):
             buff.append(serNum[i])
 
-        (status, backData, backLen) = self.Communicate_MFRC522(self.PCD_AUTHENT, buff)
+        (status, backData, backLen) = self.Communicate_MFRC522(
+            self.PCD_AUTHENT, buff)
         return status
-
 
     def StopCrypto_MFRC522(self):
         self.ClearBitMask_MFRC522(self.Status2Reg, 0x08)
-
 
     def Read(self, blockAddr):
         recvData = []
@@ -342,12 +339,12 @@ class MFRC522:
         recvData.append(pOut[0])
         recvData.append(pOut[1])
 
-        (status, backData, backLen) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, recvData)
+        (status, backData, backLen) = self.Communicate_MFRC522(
+            self.PCD_TRANSCEIVE, recvData)
 
         if not (status == self.MI_OK):
             print("Reading error")
         return backData
-
 
     def Write(self, blockAddr, writeData):
         buff = []
@@ -359,7 +356,8 @@ class MFRC522:
         buff.append(crc[0])
         buff.append(crc[1])
 
-        (status, backData, backLen) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, buff)
+        (status, backData, backLen) = self.Communicate_MFRC522(
+            self.PCD_TRANSCEIVE, buff)
 
         if not (status == self.MI_OK) or not (backLen == 4) or not ((backData[0] & 0x0F) == 0x0A):
             status = self.MI_ERR
@@ -375,13 +373,13 @@ class MFRC522:
             buff.append(crc[0])
             buff.append(crc[1])
 
-            (status, backData, backLen) = self.Communicate_MFRC522(self.PCD_TRANSCEIVE, buff)
+            (status, backData, backLen) = self.Communicate_MFRC522(
+                self.PCD_TRANSCEIVE, buff)
 
             if not (status == self.MI_OK) or not (backLen == 4) or not ((backData[0] & 0x0F) == 0x0A):
                 status = self.MI_ERR
                 print("Writing error")
         return status
-
 
     def DumpClassic1K(self, key, uid):
         for i in range(64):
@@ -390,7 +388,6 @@ class MFRC522:
                 self.Read(i)
             else:
                 print("Authentication error")
-
 
     def Init(self):
         self.Reset_MFRC522()
