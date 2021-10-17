08f3a383a3555675dbee384f5eb78d2833588cbb
fintzd <35842457+fintzd@users.noreply.github.com>
2021-10-17 11:18:22 +0200

    Update write_MFRC522.py

--- mfrc522/write_MFRC522.py
+++ mfrc522/write_MFRC522.py
@@ -5,17 +5,17 @@
 #    Copyright 2021
 #
 #    This file is part of MFRC522-Python.
-# 
+#
 #    MFRC522-Python is a simple Python implementation for the MFRC522 NFC Card Reader
 #    for the Raspberry Pi.
 #
-#    This is a free software: 
-#    You can redistribute it and/or modify it under the terms of the GNU Lesser General 
-#    Public License as published by the Free Software Foundation, either version 3 of 
+#    This is a free software:
+#    You can redistribute it and/or modify it under the terms of the GNU Lesser General
+#    Public License as published by the Free Software Foundation, either version 3 of
 #    the License, or (at your option) any later version.
 #
 #    MFRC522-Python is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-#    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #    See the GNU Lesser General Public License for more details.
 
 
@@ -38,7 +38,6 @@ class Write_KEY:
             n = n * 256 + uid[i]
         return n
 
-
     def write_info(self, data):
         (status, TagType) = self.READER.Request_MFRC522(self.READER.PICC_REQIDL)
         if status != self.READER.MI_OK:
@@ -49,22 +48,23 @@ class Write_KEY:
             return None, None
 
         self.READER.SelectTag_MFRC522(uid)
-        status = self.READER.Auth_MFRC522(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+        status = self.READER.Auth_MFRC522(
+            self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
 
         self.READER.Read(11)
 
         if status == self.READER.MI_OK:
-          buff = bytearray()
-          buff.extend(bytearray(data.ljust(len(self.DATALOCATION) * 16).encode('ascii')))
-          i = 0
-          for block_num in self.DATALOCATION:
-            self.READER.Write(block_num, buff[(i*16):(i+1)*16])
-            i += 1
+            buff = bytearray()
+            buff.extend(bytearray(data.ljust(
+                len(self.DATALOCATION) * 16).encode('ascii')))
+            i = 0
+            for block_num in self.DATALOCATION:
+                self.READER.Write(block_num, buff[(i*16):(i+1)*16])
+                i += 1
 
         self.READER.StopCrypto_MFRC522()
         r_uid = self.uid_to_num(uid)
         return r_uid, data[0:(len(self.DATALOCATION) * 16)]
-
 
     def write_loop(self, input):
         uid, data = self.write_info(input)
