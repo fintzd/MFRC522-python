6f035ca79698d9c9e146a8184194d49a1d44291e
fintzd <35842457+fintzd@users.noreply.github.com>
2021-10-17 16:05:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="MFRC522",
+    version="0.0.1",
+    author="fintzd",
+    author_email="fintzd@tuta.io",
+    description="Python3 library to read/write RFID tags via the MFRC522 RFID module.",
+    url="https://github.com/fintzd/MFRC522-python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'RPi.GPIO',
+        'spidev'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        'Topic :: System :: Hardware',
+    ],
+)
